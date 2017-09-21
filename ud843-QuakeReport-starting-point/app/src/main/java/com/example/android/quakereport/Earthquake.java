62e7c935f6c7eca87b5a1516e9da396334d8eac0
Jatin Kansal <kansaljatin95@gmail.com>
2017-09-22 01:09:47 +0530

    commit

---
+++ ud843-QuakeReport-starting-point/app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -0,0 +1,33 @@
+package com.example.android.quakereport;
+
+import static com.example.android.quakereport.R.id.magnitude;
+
+/**
+ * Created by jatin on 9/1/2017.
+ */
+
+public class Earthquake {
+    private double mMagnitude;
+
+    private String mLocation;
+
+    private long mTimeInMilliseconds;
+
+    private String mUrl;
+
+    public Earthquake(double magnitude,String location,long timeInMilliseconds,String url) {
+        mMagnitude=magnitude;
+        mLocation = location;
+        mTimeInMilliseconds = timeInMilliseconds;
+        mUrl=url;
+    }
+
+    public double getMagnitude(){return mMagnitude;}
+
+
+    public String getLocation(){return mLocation;}
+
+    public long getTimeInMilliseconds(){return mTimeInMilliseconds;}
+
+    public String getUrl(){return mUrl;}
+}
